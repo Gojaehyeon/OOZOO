b7dd0b161c0a1004ee5a678037868bacfdf39182
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-07-07 19:37:09 +0900

    feat: 기본 세팅

---
+++ OOZOO/OOZOO/OOZOOApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOZOOApp.swift
+//  OOZOO
+//
+//  Created by Gojaehyun on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct OOZOOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OOZOO/OOZOOTests/OOZOOTests.swift
@@ -0,0 +1,17 @@
+//
+//  OOZOOTests.swift
+//  OOZOOTests
+//
+//  Created by Gojaehyun on 7/7/25.
+//
+
+import Testing
+@testable import OOZOO
+
+struct OOZOOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
